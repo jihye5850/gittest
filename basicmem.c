6b015b17bb6a322ea2dad539f9236f8601afd3aa
root <root@student-vm.(none)>
2016-08-25 10:15:51 +0900

    size modified

--- basicmem.c
+++ basicmem.c
@@ -12,11 +12,11 @@ void kmalloc_test( void )
     printk( "kmalloc test\n" );
     
 	/* TODO: 1024 사이즈 만큼 kmalloc로 buff에 할당하기 */
-    allocSize = 1024;
+    allocSize = 4096;
 	buff = kmalloc(allocSize, GFP_KERNEL);
 	if( buff != NULL )
     {
-        sprintf( buff, "test memory\n" );
+        sprintf( buff, "kmalloc test memory\n" );
         printk( buff );
     	printk("test memory = %lu\n",allocSize);
         kfree( buff );
